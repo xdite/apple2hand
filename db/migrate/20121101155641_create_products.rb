735099d36ded9b923b92bb1f97a79f29189a0435
xdite <xdite@rocodev.com>
2012-11-02 00:19:04 +0800

    add products

--- db/migrate/20121101155641_create_products.rb
+++ db/migrate/20121101155641_create_products.rb
@@ -1,7 +1,18 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-
+      t.integer :category_id
+      t.string  :product_spec
+      t.date    :apple_care_expired_at
+      t.boolen  :has_invoice, :default => false
+      t.text    :accessories
+      t.string  :description
+      t.string  :extenal_link
+      t.string  :contact_type
+      t.string  :trad_area
+      t.string  :trade_type
+      t.string  :trad_price
+      t.text    :extra_note
       t.timestamps
     end
   end
