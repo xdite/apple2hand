1856d4062a5dddc7cb9ee070fb66777a5d1e085a
xdite <xdite@rocodev.com>
2012-11-02 00:10:57 +0800

    template

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,29 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+    :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+
+
+  def bind_service(response)
+    # fb
+    provider = response["provider"]
+    uid = response["uid"]
+    update_attribute(:fb_id, uid)
+  end
+
+  def facebook_user?
+    fb_id.to_i > 0
+  end
+
+
+  def admin?
+    return true if Setting.admin_emails.include?(self.email)
+    return false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Apple2hand::Application.routes.draw do
-  devise_for :users
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  resources :users
+
+  namespace :admin do
+    resources :users
+  end
 
   root :to => 'high_voltage/pages#show', :id => 'welcome'
 
