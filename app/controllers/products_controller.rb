a13bb834ab54e16ffa6a620a1f1284bf625015d3
xdite <xdite@rocodev.com>
2012-11-02 01:02:41 +0800

    products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,36 @@ class ProductsController < ApplicationController
     @product = current_user.products.build
   end
 
+
+  def create
+    @product = current_user.products.build(params[:product])
+    if @product.save
+      redirect_to product_pat(product)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = current_user.products.find(params[:id])
+
+  end
+
+  def update
+    @product = current_user.products.find(params[:id])
+    if product.update_attribute(params[:product])
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def index
+    @products = Product.paginate( :page => params[:page], :per_page => 15 )
+  end
+
 end
