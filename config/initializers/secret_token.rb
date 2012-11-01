d28b2be9e2c0f9af063d28638c551401de9802a9
xdite <xdite@rocodev.com>
2012-11-01 23:58:48 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apple2hand::Application.config.secret_token = 'f914d24ac8a0f96b0f3a60194fd8b9893189307eaa533d06c47bd56eed5bcece999d3498784305121526fea32b07ebf46a66876175f5b599f2db788be4448a1b'
